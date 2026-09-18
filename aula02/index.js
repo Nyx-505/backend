4dbf261befe940eeeeb9f874bbd667e8008a3df1
any <anyyy3458@gmail.com>
2026-09-18 10:30:58 -0300

    backs and end

---
+++ aula02/index.js
@@ -0,0 +1,18 @@
+// INICIO BACKEND - JAVASCRIPT
+
+// VAIÁVEIS
+
+//ANTIGO !!!!!!!!!!!!!!!, pode ser redeclarado e mudar o valor.
+var idade = 10
+var idade = 20
+
+//Mais moderno, não pode mudar o valor, mas pode ser redeclarado
+let nome = "fulano"
+nome - "ciclano" ; //permitido
+
+let nome = "beltrano"; //erro
+
+
+//Nao mudu mudar o valor
+const pi = 3.14
+
---
+++ aula02/insex.js
@@ -0,0 +1,45 @@
+var (cor) = "azul"
+var (cor) = "rosa"
+consol.log (cor)
+
+let (fruta) = "manga"
+let (fruta) = "melancia"
+console.log (fruta)
+
+const pi = 3.14;
+//pi = 3.15; erro
+console.log(pi); 
+// pi valor fixo
+
+//-----------------------
+//FORMAS DE ESCREVER
+//----------------------
+
+
+//um codigo
+//-----------------------
+
+//CAMEL CASE  ------* A mais famosa
+// primeira palavra minuscula
+// palavras seguintes minusculas
+
+//--------------------------------
+
+//let nomeCOMPLETO; Let todousuario
+
+//_______________________________________
+//PASCALCASE
+// todas palavras começem com maiuscula
+
+//class UsuarioSistema {
+// constructor (Nome, idade){
+//this.nome;
+// this.idade;
+//}
+//};
+
+//______________________________________
+//SNAKE CASE
+// palavras separasas por underscore _
+
+// let nome_completo;
